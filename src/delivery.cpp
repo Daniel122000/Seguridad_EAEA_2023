45ec1fb901f91da1aa9b7973edba0b5b9651dab5
Mich04c2 <michael.venegasrojas@ucr.ac.cr>
2023-10-21 22:47:56 -0600

    Creado el programa encargado de entregar los mensajes.

---
+++ src/delivery.cpp
@@ -0,0 +1,39 @@
+#include "clientEAEA.hpp"
+#include "argsVerifier.hpp"
+
+#define IP_ADDR "127.0.0.1"
+
+std::string prepare_delivery_message(char const *argv[]){
+    std::string x = argv[0];
+    std::string user = argv[1];
+    std::string signature = argv[2];
+    std::string plainText = argv[3];
+
+    std::string message = x.append(" ");
+    message.append(user).append(" ").append(signature).append(" ").append(plainText);
+    return message;
+}
+
+void deliver_message(std::string message){
+    ClientEAEA client;
+
+    client.convert_Addresses(IP_ADDR);
+    client.create_Connection();
+    client.sendMessage(message.c_str());
+    std::cout << "Message sent to " << IP_ADDR << std::endl;
+    client.endConnection();
+}
+
+int main(int argc, char const *argv[])
+{
+    ArgsVerifier verifier;
+    if(!verifier.verify_arguments_delivery(argc, argv)){
+        return 1;
+    }
+    
+    std::string message = prepare_delivery_message(argv);
+
+    deliver_message(message);
+
+    return 0;
+}
