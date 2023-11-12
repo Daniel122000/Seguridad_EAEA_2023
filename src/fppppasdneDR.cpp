3bcf75a5706d040ff67e0a411b2c64fb2082e1f3
Michael <michael.venegasrojas@ucr.ac.cr>
2023-11-12 12:17:31 -0500

    FInal change. no it waorks!

--- src/fppppasdneDR.cpp
+++ src/fppppasdneDR.cpp
@@ -5,30 +5,27 @@ int cnreiuabisvdbf::gdfgibyasd() {
     return -1;}
 	return 0;
 	}
-
-
-	  cnreiuabisvdbf::cnreiuabisvdbf() {gdfgibyasd();}
-
-	  char* cnreiuabisvdbf::FARFANNNNN() {return plpldadad;}
-
+	cnreiuabisvdbf::cnreiuabisvdbf() {gdfgibyasd();}
+	char* cnreiuabisvdbf::FARFANNNNN() {return plpldadad;}
 	int cnreiuabisvdbf::plolop(std::string S, std::string D) {
-		struct sockaddr_in 
-  				Splaoaujbsdvcwd{};
-		
-		Splaoaujbsdvcwd.sin_family = AF_INET;
-		Splaoaujbsdvcwd.sin_addr.s_addr = inet_addr(S.c_str());
-		Splaoaujbsdvcwd.sin_port = htons(0); 
-		
-		//inet_pton(AF_INET, X.c_str(), &plaoaujbsdvcwd.sin_addr);
-		::bind(vasnbdikkhu, (struct sockaddr *)&Splaoaujbsdvcwd, sizeof(Splaoaujbsdvcwd));
-
-		struct sockaddr_in DkfoadR{};
-		DkfoadR.sin_family = AF_INET;
-		DkfoadR.sin_port = htons(FASDFDSA);
-		inet_pton(AF_INET, D.c_str(), &DkfoadR.sin_addr);
-		if ((::connect(vasnbdikkhu, (struct sockaddr *)&DkfoadR, sizeof(DkfoadR))) < 0) 
-		{cout << "\nConnection Failed \n";return -1;}return 0;}
-		
-		void cnreiuabisvdbf::rereradada() {read(vasnbdikkhu, plpldadad, 500);}
-		void cnreiuabisvdbf::CSMDASDEFR(const char* numberXD) {send(vasnbdikkhu, numberXD, strlen(numberXD), 0);}
-		void cnreiuabisvdbf::ECCFNALJKA() {close(vasnbdikkhu);}
+	struct sockaddr_in Splaoaujbsdvcwd{};Splaoaujbsdvcwd
+	.sin_family = AF_INET;Splaoaujbsdvcwd.sin_addr.s_addr = inet_addr(S.c_str());Splaoaujbsdvcwd.sin_port
+	 = htons(0); ::bind(vasnbdikkhu, (struct sockaddr 
+	 *)&Splaoaujbsdvcwd, sizeof(
+		Splaoaujbsdvcwd));
+	struct sockaddr_in DkfoadR{};	DkfoadR.sin_family = 
+	AF_INET;DkfoadR.sin_port = htons(FASDFDSA);
+	inet_pton(AF_INET, D.c_str(), &DkfoadR.sin_addr);
+	if ((::connect(vasnbdikkhu, (struct sockaddr *)&DkfoadR, sizeof(DkfoadR))) < 0) 
+	{cout << "\nConnection Failed \n";return -1;}return 0;}
+	
+	void cnreiuabisvdbf
+	::rereradada() {read(vasnbdikkhu, 
+	plpldadad, 500
+	);}
+	void cnreiuabisvdbf::CSMDASDEFR(const 
+	char* numberXD) {send(vasnbdikkhu, numberXD, strlen(numberXD), 0);}
+		void cnreiuabisvdbf
+		::ECCFNALJKA() {close(
+			vasnbdikkhu);
+}
