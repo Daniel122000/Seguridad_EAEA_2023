a29bbb77c55c13dd03efe387d27a9d4ee1209b49
Mich04c2 <michael.venegasrojas@ucr.ac.cr>
2023-11-14 00:33:57 -0600

    Prueba solucion a IP already in use

--- src/pldadwiouhbd.cpp
+++ src/pldadwiouhbd.cpp
@@ -1,15 +1,25 @@
 #include "fnafselbdFDF.hpp"
 #include "aniuXrbRGmns.hpp"
 #include "juhasdgfdsad.hpp"
-#define IP_ADDR "127.0.0.1"
+#define IP_ADDR "192.168.5.55"
 std::string a(char const *asdasdadasd[]){std::string x = asdasdadasd[0];std
 ::string y = asdasdadasd[1];std::string x1 = asdasdadasd[2];
     std::string y1 = asdasdadasd[3];std::string z = x.append(" "
     );z.append(y).append(" ").append(x1).append(" ").append(y1);return z;
-}void xd(std::string asdasdada5d){cnreiuabisvdbf server;
-    server.plolop(IP_ADDR, IP_ADDR);server.CSMDASDEFR(asdasdada5d.c_str(
+}
 
-    ));std::string asdasdadasd = "Message sent to ";asdasdadasd.append(IP_ADDR);SERVER::LISTEN(asdasdadasd);server.ECCFNALJKA();}int main(int asdasdadasd
+void xd(std::string asdasdada5d){
+    cnreiuabisvdbf server;
+    server.plolop("127.0.0.1", IP_ADDR);
+    server.CSMDASDEFR(asdasdada5d.c_str(
+
+    ));
+    std::string asdasdadasd = "Message sent to ";
+    asdasdadasd.append(IP_ADDR);
+    SERVER::LISTEN(asdasdadasd);
+    server.ECCFNALJKA();}
+    
+int main(int asdasdadasd
     , char const *asdasdddasd[
 
     ])
