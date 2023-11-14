9a1e40f45cdd5dac0fe2251c71e513fa8ef8c89e
Mich04c2 <michael.venegasrojas@ucr.ac.cr>
2023-11-14 01:47:17 -0600

    Ultimo cambio para obfuscacion.

--- src/pldadwiouhbd.cpp
+++ src/pldadwiouhbd.cpp
@@ -2,26 +2,24 @@
 #include "aniuXrbRGmns.hpp"
 #include "juhasdgfdsad.hpp"
 #define IP_ADDR "192.168.5.55"
-std::string a(char const *asdasdadasd[]){std::string x = asdasdadasd[0];std
-::string y = asdasdadasd[1];std::string x1 = asdasdadasd[2];
+std::string a(char const *asdasdadasd[
+    
+]){std::string x = asdasdadasd[0];std
+::string y = asdasdadasd[1
+];std::string x1 = asdasdadasd[2];
     std::string y1 = asdasdadasd[3];std::string z = x.append(" "
-    );z.append(y).append(" ").append(x1).append(" ").append(y1);return z;
+    );z.append(y).append(" ").
+    append(x1).append(" ").append(y1);return z;
 }
 
 int xd(std::string asdasdada5d){
-    cnreiuabisvdbf server;
-    int r = server.plolop("127.0.0.1", IP_ADDR);
+    cnreiuabisvdbf server;    int r = server.plolop(
+        "127.0.0.1", IP_ADDR);
     if(r == 0){server.CSMDASDEFR(asdasdada5d.c_str(
-
-    ));
-    std::string asdasdadasd = "Message sent to ";
+    ));    std::string asdasdadasd = "Message sent to ";
     asdasdadasd.append(IP_ADDR);
-    SERVER::LISTEN(asdasdadasd);
-    server.ECCFNALJKA();}else{
-        SERVER::LISTEN("No se pudo enviar el mensaje. Intente de nuevo.");
-    }}
-    
-int main(int asdasdadasd
+    SERVER::LISTEN(asdasdadasd);    server.ECCFNALJKA();}else{
+        SERVER::LISTEN("No se pudo enviar el mensaje. Intente de nuevo."); }}    int main(int asdasdadasd
     , char const *asdasdddasd[
 
     ])
